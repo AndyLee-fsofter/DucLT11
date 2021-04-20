b84e41758f06d05ad00f4046b7f82aff941aa619
AndyLee-fsofter <77046851+AndyLee-fsofter@users.noreply.github.com>
2021-04-20 21:37:00 +0700

    Create Main.java

---
+++ Bai2/src/com/luvina/main/Main.java
@@ -0,0 +1,14 @@
+package com.luvina.main;
+
+import com.luvina.agecalculate.Person;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		p1.ageCalculate(1993);
+		p1.display();
+
+	}
+
+}
